ee30ab3b1b404a7bd4bd3a754705d69f13292e73
Steven Ramsay <steven.ramsay@live.com>
2016-04-14 16:38:41 +0100

    Added PUtils.dll locally.

--- Rethink/Scripts/load-references-debug.fsx
+++ Rethink/Scripts/load-references-debug.fsx
@@ -1,7 +1,7 @@
 // Warning: generated file; your changes could be lost when a new file is generated.
 #I __SOURCE_DIRECTORY__
 #r "../../packages/FSharp.Compiler.Service.2.0.0.7/lib/net45/FSharp.Compiler.Service.dll"
-#r "../../../PUtils/bin/Release/PUtils.dll"
 #r "System.Core.dll"
 #r "System.dll"
 #r "System.Numerics.dll"
+#r "../PUtils.dll"
