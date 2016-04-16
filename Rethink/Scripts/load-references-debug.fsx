09d1bfce766ad367fd9fe0a8f1cc7dc383d63fd4
Robin Neatherway <robin.neatherway@gmail.com>
2016-04-17 00:24:27 +0100

    Use paket for packages

--- Rethink/Scripts/load-references-debug.fsx
+++ Rethink/Scripts/load-references-debug.fsx
@@ -1,6 +1,6 @@
 // Warning: generated file; your changes could be lost when a new file is generated.
 #I __SOURCE_DIRECTORY__
-#r "../../packages/FSharp.Compiler.Service.2.0.0.7/lib/net45/FSharp.Compiler.Service.dll"
+#r "../../packages/FSharp.Compiler.Service/lib/net45/FSharp.Compiler.Service.dll"
 #r "System.Core.dll"
 #r "System.dll"
 #r "System.Numerics.dll"
